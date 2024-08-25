ca67480f97d5817a24d67aaf82c99f1cd5391627
Sin Break <js882829@gmail.com>
2024-08-25 18:07:35 +0800

    Update uploader.py
    update

--- uploader.py
+++ uploader.py
@@ -30,6 +30,26 @@ def upload_docker_images(source_image, target_image, registry_url, username, pas
     logout_command = f"docker logout {registry_url}"
     run_command(logout_command)
 
+def read_json_file(file_path):
+    """ 读取 JSON 文件并返回内容。 """
+    try:
+        with open(file_path) as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+    except json.JSONDecodeError:
+        print(f"解析 '{file_path}' 时出错。")
+        sys.exit(1)
+
+def write_json_file(file_path, data):
+    """ 将数据写入 JSON 文件。 """
+    try:
+        with open(file_path, 'w') as file:
+            json.dump(data, file, indent=2)
+    except IOError as e:
+        print(f"写入 '{file_path}' 时出错: {e}")
+        sys.exit(1)
+
 def main():
     if len(sys.argv) != 4:
         print("用法: python uploader.py <username> <password> <registry_url>")
@@ -39,34 +59,28 @@ def main():
     password = sys.argv[2]
     registry_url = sys.argv[3]
 
-    synced_images = []
-    
-    # 读取 JSON 文件中的镜像
-    try:
-        with open('images.json') as file:
-            images = json.load(file)
-    except FileNotFoundError:
-        print("文件 'images.json' 未找到。")
-        sys.exit(1)
-    except json.JSONDecodeError:
-        print("解析 'images.json' 时出错。")
-        sys.exit(1)
-    
+    # 读取已同步的镜像
+    synced_images = read_json_file('synced_images.json')
+
+    # 读取待同步的镜像
+    images = read_json_file('images.json')
+
+    # 创建一个集合来避免重复
+    synced_images_set = {json.dumps(img, sort_keys=True) for img in synced_images}
+
     for image in images:
-        try:
-            print(f"处理镜像: {image['target']}")
-            upload_docker_images(image["source"], image["target"], registry_url, username, password)
-            synced_images.append(image)
-        except Exception as e:
-            print(f"上传镜像 {image['target']} 失败: {e}")
+        image_str = json.dumps(image, sort_keys=True)
+        if image_str not in synced_images_set:
+            try:
+                print(f"处理镜像: {image['target']}")
+                upload_docker_images(image["source"], image["target"], registry_url, username, password)
+                synced_images.append(image)
+                synced_images_set.add(image_str)
+            except Exception as e:
+                print(f"上传镜像 {image['target']} 失败: {e}")
 
     # 将成功上传的镜像写入 synced_images.json
-    try:
-        with open('synced_images.json', 'w') as file:
-            json.dump(synced_images, file, indent=2)
-        print("已成功写入 synced_images.json")
-    except IOError as e:
-        print(f"写入 'synced_images.json' 时出错: {e}")
+    write_json_file('synced_images.json', synced_images)
 
 if __name__ == "__main__":
     main()
